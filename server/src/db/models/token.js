fb8b4c600f86d06cfdf480940193fb8f5a421313
Houzifa Habbo <h-hebbo@hotmail.com>
2023-12-21 23:05:10 +0300

    add new features for user and designer

---
+++ server/src/db/models/token.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const rand = function () {
+  return Math.random().toString(36).substr(2); // remove `0.`
+};
+
+const token = function () {
+  return rand() + rand() + rand() + "-" + rand() + rand() + rand(); // to make it longer
+};
+
+const tokenSchema = mongoose.Schema({
+  token: {
+    type: String,
+    required: true,
+    default: token,
+  },
+  model_type: {
+    type: String,
+    enum: ["User", "Designer"],
+    required: true,
+  },
+  account: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "model_type",
+    required: true,
+  },
+  token_expiry: {
+    type: Date,
+    default: Date.now,
+    expiers: 10 * 60 * 1000,
+  },
+});
+
+module.exports = mongoose.model("Token", tokenSchema);

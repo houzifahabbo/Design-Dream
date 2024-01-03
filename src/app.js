39e450f8a2c151126abc24256b6d3278e629967a
Houzifa Habbo <h-hebbo@hotmail.com>
2024-01-03 11:06:53 +0300

    trying to host

--- src/app.js
+++ src/app.js
@@ -33,6 +33,12 @@ middleware.forEach((item) => {
   app.use(item);
 });
 
+app.get("/test", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 app.use("/api", apiRoutes);
 
 app.use("/", frontendRoutes);

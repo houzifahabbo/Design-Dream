2bb9d1a6ab7804375422e80f7932760a97b96a0e
Houzifa Habbo <h-hebbo@hotmail.com>
2024-01-03 11:01:53 +0300

    trying to host

--- src/app.js
+++ src/app.js
@@ -29,6 +29,15 @@ app.use("/api", apiRoutes);
 
 app.use("/", frontendRoutes);
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 if (process.env.NODE_ENV !== "test") {
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);

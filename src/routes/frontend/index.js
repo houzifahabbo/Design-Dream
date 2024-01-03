3d8743a3d49b54a3fff0b895b43c0d077b5e3dac
Houzifa Habbo <h-hebbo@hotmail.com>
2024-01-03 10:37:44 +0300

    changed file struct

--- src/routes/frontend/index.js
+++ src/routes/frontend/index.js
@@ -8,28 +8,29 @@ import { dirname } from "../../app.js";
 import path from "path";
 const routes = express.Router();
 
-// routes.get("/", (req, res) => {
-//   res.sendFile(path.join(dirname, "../public", `index.html`));
-// });
+routes.get("/", (req, res) => {
+  res.send(path.join(dirname, "../public", `index.html`));
+});
 
-// routes.get("/signin", (req, res) => {
-//   res.sendFile(path.join(dirname, "../public", `signin.html`));
-// });
+routes.get("/signin", (req, res) => {
+  res.send(path.join(dirname, "../public", `signin.html`));
+});
 
-// routes.get("/signup", (req, res) => {
-//   res.sendFile(path.join(dirname, "../public", `signup.html`));
-// });
+routes.get("/signup", (req, res) => {
+  // res.sendFile(path.join(dirname, "../public", `signup.html`));
+  res.json({ message: "signup" });
+});
 
-// routes.get("about-us", (req, res) => {
-//   res.sendFile(path.join(dirname, "../public", `about-us.html`));
-// });
-// routes.get("contact", (req, res) => {
-//   res.sendFile(path.join(dirname, "../public", `contact.html`));
-// });
-// routes.use("/user", userRoutes);
-// routes.use("/designer", designerRoutes);
-// routes.use("/payment", paymentRoutes);
-// routes.use("/order", orderRoutes);
-// routes.use("/rating", ratingRoutes);
+routes.get("about-us", (req, res) => {
+  res.sendFile(path.join(dirname, "../public", `about-us.html`));
+});
+routes.get("contact", (req, res) => {
+  res.sendFile(path.join(dirname, "../public", `contact.html`));
+});
+routes.use("/user", userRoutes);
+routes.use("/designer", designerRoutes);
+routes.use("/payment", paymentRoutes);
+routes.use("/order", orderRoutes);
+routes.use("/rating", ratingRoutes);
 
 export default routes;
